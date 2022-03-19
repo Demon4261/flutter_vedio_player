24573c7666079d938b7f1f0f24f976daec401c95
Ansh Malik <anshmalik279@gmail.com>
2022-03-19 22:59:03 +0530

    day 1 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: color.AppColor.homePageBackground,
       body: Container(
-          padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+          padding: const EdgeInsets.only(top: 65, left: 20, right: 20),
           child: Column(
             children: [
               Row(
@@ -43,6 +43,141 @@ class HomePage extends StatelessWidget {
                   )
                 ],
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "your program",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: color.AppColor.homePageSubtitle,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Text(
+                    "Details",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: color.AppColor.homePageDetail,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    size: 20,
+                    color: color.AppColor.homePageIcons,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          color.AppColor.gradientFirst.withOpacity(0.8),
+                          color.AppColor.gradientSecond.withOpacity(0.9),
+                        ],
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(80),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(5, 10),
+                          color: color.AppColor.gradientSecond.withOpacity(0.2),
+                          blurRadius: 20,
+                        ),
+                      ]),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 25, left: 20, right: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Next workout",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: color.AppColor.homePageContainerTextSmall),
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "Legs Toning",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: color.AppColor.homePageContainerTextBig),
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "and Glutes Workout",
+                          style: TextStyle(
+                              fontSize: 25,
+                              // fontWeight: FontWeight.bold,
+                              color: color.AppColor.homePageContainerTextBig),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.timer,
+                                  size: 20,
+                                  color:
+                                      color.AppColor.homePageContainerTextSmall,
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text("60 min",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: color.AppColor
+                                            .homePageContainerTextSmall)),
+                              ],
+                            ),
+                            Expanded(child: Container()),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 20,
+                                      offset: const Offset(4, 8),
+                                      color: color.AppColor.gradientFirst)
+                                ],
+                              ),
+                              child: Icon(
+                                Icons.play_circle_fill,
+                                color: color.AppColor.homePageContainerTextBig,
+                                size: 60,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ))
             ],
           )),
     );
